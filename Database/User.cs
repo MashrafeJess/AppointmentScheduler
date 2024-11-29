aa4f925604f654d33fc66df87d0f2a721a3ab946
Student <mashrafeiubat@gmail.com>
2024-11-29 18:35:32 +0600

    Class 5

---
+++ Database/User.cs
@@ -0,0 +1,12 @@
+﻿namespace Database
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Mail { get; set; }
+        public string Password { get; set; }
+
+        public string Designation{ get; set; }
+        public string Mode { get; set; }
+    };
+}
