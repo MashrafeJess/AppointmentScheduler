eecb4d5969f79b9e4d2d3a12761124341d4c269c
MashrafeJess <mashrafeiubat@gmail.com>
2024-11-30 19:44:42 +0600

    Class 6

---
+++ Database/Model/Appointment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database.Model
+{
+    internal class Appointment
+    {
+        public int Serial { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+        public DateTime Appoint { get; set; }
+
+        public int RoomNumber { get; set; }
+    }
+}
--- Database/Model/User.cs
+++ Database/Model/User.cs
@@ -1,12 +1,12 @@
-﻿namespace Database
+﻿namespace Database.Model
 {
     public class User
     {
         public string Name { get; set; }
         public string Mail { get; set; }
-        public string HashPassword { get; set; }
+        public string Password { get; set; }
 
-        public string Designation{ get; set; }
+        public string Designation { get; set; }
         public string Mode { get; set; }
     };
 }
